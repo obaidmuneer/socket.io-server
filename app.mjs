964fdfc882ec7d1dfc804ced3e02747d0bbc82f1
Obaid Muneer <96944978+obaidmuneer@users.noreply.github.com>
2022-11-05 18:57:39 +0500

    Update app.mjs

--- app.mjs
+++ app.mjs
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 // import cors from "cors";
 
 const app = express();
-const port = 8080 || process.env.port;
+const port = process.env.PORT || 8080 ;
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
