b6d2b57fd61105c21de3d8e2fac7f73e201527b6
obaidmuneer <obaidmuneer55@gmail.com>
2023-02-25 17:35:23 +0500

    some changes

--- app.mjs
+++ app.mjs
@@ -2,17 +2,17 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import mongoose from "mongoose";
-// import cors from "cors";
+import * as dotenv from 'dotenv'
 
 const app = express();
 const port = process.env.PORT || 8080 ;
 const server = createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
+dotenv.config()
 
 // app.use(cors())
 
-let dbUri =
-  "mongodb+srv://obaidmuneer:Abc123@cluster0.jop1spc.mongodb.net/?retryWrites=true&w=majority";
+let dbUri = process.env.DB_URI
 mongoose.connect(dbUri);
 
 let chatSchema = new mongoose.Schema({
